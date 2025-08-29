2bf41d107f654fbf113ada9905acba972bab76e6
abrieant <54375775+abrieant@users.noreply.github.com>
2025-08-29 10:24:22 -0400

    create NLE variable

--- 6.MergeAllData.R
+++ 6.MergeAllData.R
@@ -42,3 +42,62 @@ dfmerged <- dfmerged %>%
 
 write.csv(dfmerged, "~/GitHub/pubertybrain/dfmerged.csv", row.names = TRUE)
 
+#######merge in adversity data
+
+dfmerged <- read.csv("/Users/ab3377/Library/CloudStorage/OneDrive-UniversityofVermont/OneDrive/Manuscripts/reg report DCN/pubertybrain/dfmerged.csv")
+
+NLE <- read_tsv("/Users/ab3377/Library/CloudStorage/OneDrive-UniversityofVermont/OneDrive/ABCD_6.0/rawdata/phenotype/mh_y_ple.tsv") %>%
+  filter(session_id == "ses-01A")
+
+###customized NLE code
+
+library(dplyr)
+
+compute_negative_events <- function(df) {
+  event_vars    <- paste0("mh_y_ple_", sprintf("%03d", 1:25))
+  exp_vars      <- paste0("mh_y_ple__exp_", sprintf("%03d", 1:25))
+  pastyear_vars <- paste0("mh_y_ple_", sprintf("%03d", 1:25), "__01")
+  
+  df %>%
+    rowwise() %>%
+    mutate(
+      total_bad_events = {
+        events <- suppressWarnings(as.numeric(unlist(across(all_of(event_vars)))))
+        exps   <- suppressWarnings(as.numeric(unlist(across(all_of(exp_vars)))))
+        # recode special missing values to NA
+        events[events %in% c(444, 777, 999)] <- NA
+        exps[exps %in% c(444, 777, 999)]     <- NA
+        if(sum(is.na(events)) > 5) NA
+        else sum(events == 1 & exps == 2, na.rm = TRUE)
+      },
+      total_bad_events_baseline = {
+        events <- suppressWarnings(as.numeric(unlist(across(all_of(event_vars)))))
+        exps   <- suppressWarnings(as.numeric(unlist(across(all_of(exp_vars)))))
+        pastyr <- suppressWarnings(as.numeric(unlist(across(all_of(pastyear_vars)))))
+        # recode special missing values to NA
+        events[events %in% c(444, 777, 999)] <- NA
+        exps[exps %in% c(444, 777, 999)]     <- NA
+        pastyr[pastyr %in% c(444, 777, 999)] <- NA
+        if(sum(is.na(events)) > 5) NA
+        else sum(events == 1 & exps == 2 & pastyr == 0, na.rm = TRUE)
+      }
+    ) %>%
+    ungroup()
+}
+
+NLE <- compute_negative_events(NLE)
+
+
+NLE$total_NLE_baseline <- NLE$mh_y_ple_neg_count
+
+####
+#check 5.1
+
+path <- '/Users/ab3377/Library/CloudStorage/OneDrive-UniversityofVermont/OneDrive/FERN Lab/abcd-data-release-5.1/core'
+
+NLE5 <- read_csv(paste(path, 'mental-health/mh_y_le.csv', sep='/'), col_names=TRUE) %>% 
+  filter(eventname == "1_year_follow_up_y_arm_1") %>% 
+  dplyr::select(src_subject_id, ple_y_ss_total_bad, ple_y_ss_affected_bad_mean)
+
+table(NLE5$ple_y_ss_total_bad)
+
